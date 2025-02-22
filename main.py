4e12dbb66d042ae6b8d2c7eb06a39c548387b6c8
Jonathan <ph271828@gmail.com>
2025-02-22 20:56:01 +0900

    basic usage: llm with prompt

--- main.py
+++ main.py
@@ -1,15 +1,28 @@
 from langchain_ollama import ChatOllama
+from langchain_core.messages import AIMessage, HumanMessage, SystemMessage
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 
 llm = ChatOllama(
     model="llama3",
     temperature=0,
 )
 
-messages = [
-    ("system", "You are a helpful translator. Translate the user sentence to French."),
-    ("human", "I love programming."),
-    ("system", "J\'adore le programmation.\n\n(Note: \"programming\" can also be translated as \"informatique\" or \"programmation\", but in this context, I chose to use the more informal and conversational translation \"le programmation\")"),
-    ("human", "Have a nice trip")
-]
-resp = llm.invoke(messages)
+prompt = ChatPromptTemplate.from_messages(
+    [
+        SystemMessage(
+            content="You are a helpful assistant that translates English to German."
+        ),
+        MessagesPlaceholder(variable_name="messages"),
+    ]
+)
+
+chains = prompt | llm
+resp = chains.invoke(
+    {
+        "messages": [
+            HumanMessage(content="I love programming."),
+        ],
+    }
+)
+
 print(resp.content)
