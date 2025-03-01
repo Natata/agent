1f01d867e3abd7917a759f999fff5a0199035c77
Jonathan <ph271828@gmail.com>
2025-03-01 11:14:10 +0900

    add random number tool and update tools condition logic for multiple tools

--- agent.py
+++ agent.py
@@ -27,7 +27,8 @@ from langgraph.checkpoint.memory import MemorySaver
 
 
 MODEL_LLAMA31 = "llama3.1"
-NOTE_TOOL_CULCULATOR = "node_tool_calculator"
+NODE_TOOL_CULCULATOR = "node_tool_calculator"
+NODE_TOOL_RANDOM_NUMBER = "node_tool_random_number"
 
 # -- tools
 @tool
@@ -35,13 +36,18 @@ def multiply(a: int, b: int) -> int:
     """Multiply a and b."""
     return a * b
 
+@tool
+def random_number() -> int:
+    """Return a random number."""
+    return 123123
+
 class State(TypedDict):
     # Messages have the type "list". The `add_messages` function
     # in the annotation defines how this state key should be updated
     # (in this case, it appends messages to the list, rather than overwriting them)
     messages: Annotated[list, add_messages]
 
-def tools_condition(
+def multiple_tools_condition(
     state: Union[list[AnyMessage], dict[str, Any], BaseModel],
     messages_key: str = "messages",
 ) -> Literal["node_tool_calculator", "__end__"]:
@@ -54,27 +60,57 @@ def tools_condition(
     else:
         raise ValueError(f"No messages found in input state to tool_edge: {state}")
     if hasattr(ai_message, "tool_calls") and len(ai_message.tool_calls) > 0:
-        return NOTE_TOOL_CULCULATOR
+        print("**** ai_message.tool_calls:", ai_message.tool_calls)
+        if ai_message.tool_calls[-1]['name'] == "multiply":
+            return NODE_TOOL_CULCULATOR
+    return "__end__"
+
+def random_tools_condition(
+    state: Union[list[AnyMessage], dict[str, Any], BaseModel],
+    messages_key: str = "messages",
+) -> Literal["node_tool_random_number", "__end__"]:
+    if isinstance(state, list):
+        ai_message = state[-1]
+    elif isinstance(state, dict) and (messages := state.get(messages_key, [])):
+        ai_message = messages[-1]
+    elif messages := getattr(state, messages_key, []):
+        ai_message = messages[-1]
+    else:
+        raise ValueError(f"No messages found in input state to tool_edge: {state}")
+    if hasattr(ai_message, "tool_calls") and len(ai_message.tool_calls) > 0:
+        print("**** ai_message.tool_calls:", ai_message.tool_calls)
+        if ai_message.tool_calls[-1]['name'] == "random_number":
+            return NODE_TOOL_RANDOM_NUMBER
     return "__end__"
 
 class Agent:
     def __init__(self, model=MODEL_LLAMA31):
+        tools = [multiply, random_number]
+
         self.llm = ChatOllama(
             model=model,
             temperature=0,
-        ).bind_tools([multiply])
+        ).bind_tools(tools)
 
         graph_builder = StateGraph(State)
         # nodes
         graph_builder.add_node("assistant", self.assistant)
-        tool_node = ToolNode(tools=[multiply])
-        graph_builder.add_node(NOTE_TOOL_CULCULATOR, tool_node)
+        tool_node_multiple = ToolNode(tools=[multiply], name="node_tool_calculator")
+        graph_builder.add_node(NODE_TOOL_CULCULATOR, tool_node_multiple)
+        tool_node_random_number = ToolNode(tools=[random_number], name="node_tool_random_number")
+        graph_builder.add_node(NODE_TOOL_RANDOM_NUMBER, tool_node_random_number)
         # edges
         graph_builder.add_conditional_edges(
             "assistant",
-            tools_condition,
+            multiple_tools_condition,
         )
-        graph_builder.add_edge(NOTE_TOOL_CULCULATOR, "assistant")
+        graph_builder.add_edge(NODE_TOOL_CULCULATOR, "assistant")
+        graph_builder.add_conditional_edges(
+            "assistant",
+            random_tools_condition,
+        )
+        graph_builder.add_edge(NODE_TOOL_RANDOM_NUMBER, "assistant")
+
         graph_builder.set_entry_point("assistant")
         graph_builder.set_finish_point("assistant")
 
@@ -84,15 +120,20 @@ class Agent:
         self.graph = graph_builder.compile(checkpointer=memory)
 
         ## NOTE: comment out to show the graph
-        # try:
-        #     display(Image(self.graph.get_graph().draw_mermaid_png()))
-        # except Exception:
-        #     # This requires some extra dependencies and is optional
-        #     pass
+        try:
+            display(Image(self.graph.get_graph().draw_mermaid_png()))
+        except Exception:
+            # This requires some extra dependencies and is optional
+            pass
 
     def assistant(self, state: State):
         system_message = SystemMessage(
-            content="You are a helpful assistant. Use the 'multiply' tool only when the user explicitly asks for multiplication (e.g., 'multiply 3 and 4' or 'what is 3 times 4'). For all other questions, answer directly using your knowledge."
+            content="""
+                You are a helpful assistant. 
+                Use the 'multiply' tool only when the user explicitly asks for multiplication (e.g., 'multiply 3 and 4' or 'what is 3 times 4'). 
+                Use the 'random_number' tool only when the user explicitly asks for a random number (e.g., 'give me a random number').
+                For all other questions, answer directly using your knowledge.
+                """,
         )
         messages = state["messages"]
         response = self.llm.invoke([system_message] + messages)
@@ -107,6 +148,7 @@ class Agent:
         ):
             print("### debug event:", event)
             for value in event.values():
+                # value[-1].pretty_print()
                 response = value[-1].content
         return response
 
